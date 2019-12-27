7b465e0688925c7c7056e015f3ba67a20ae4c3b5
xuweng <2028403305@qq.com>
2019-12-27 23:13:51 +0800

    基础扎实。基础扎实。基础扎实。 年轻代和老年代使用的垃圾收集算法

---
+++ depth-jvm/src/main/java/com/jvm/study/plumbr/ParallelGC.java
@@ -0,0 +1,4 @@
+package com.jvm.study.plumbr;
+
+public class ParallelGC {
+}
--- depth-jvm/src/main/java/com/jvm/study/plumbr/SerialGC.java
+++ depth-jvm/src/main/java/com/jvm/study/plumbr/SerialGC.java
@@ -3,6 +3,9 @@ package com.jvm.study.plumbr;
 /**
  * https://github.com/cncounter/gc-handbook/blob/master/04_GC_Algorithms_Implementations.md
  * <p>
+ * Serial GC 对年轻代使用 mark-copy(标记-复制) 算法, 对老年代使用 mark-sweep-compact(标记-清除-整理)算法.
+ * 顾名思义,两者都是单线程的垃圾收集器,不能进行并行处理。两者都会触发全线暂停(STW),停止所有的应用线程。
+ * <p>
  * 可以从上面的日志片段了解到, 在GC事件中,JVM 的内存使用情况发生了怎样的变化。
  * 此次垃圾收集之前, 堆内存总的使用量为 1,619,346K。其中,年轻代使用了 629,119K。可以算出,老年代使用量为 990,227K。
  * <p>
