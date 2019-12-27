e0a5a805c5d08dc40b1df8ff387724a668d0c064
xuweng <2028403305@qq.com>
2019-12-27 22:56:02 +0800

    最好的学习方式。结合代码学习。培养对代码感觉 学习优秀代码。学习官方代码

--- depth-jvm/src/main/java/com/jvm/study/plumbr/SerialGC.java
+++ depth-jvm/src/main/java/com/jvm/study/plumbr/SerialGC.java
@@ -1,12 +1,15 @@
 package com.jvm.study.plumbr;
 
 /**
+ * 最好的学习方式。结合代码学习。培养对代码感觉
+ * 学习优秀代码。学习官方代码。
+ * <p>
  * 同时对年轻代和老年代生效
  * <p>
  * -XX:+UseSerialGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps
  */
 public class SerialGC {
     public static void main(String[] args) {
-        System.out.println(111);
+        System.out.println();
     }
 }
